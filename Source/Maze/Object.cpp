df5a7d9c898fcc4c4db4415e787056f716687efb
Mateusz Serkies <mateusz.serkies@artifexmundi.com>
2020-11-01 22:58:56 +0100

    Added ability to set position, rotation and scale of object.

--- Source/Maze/Object.cpp
+++ Source/Maze/Object.cpp
@@ -1,10 +1,10 @@
 #include "Object.h"
 #include "ResourcesManager.h"
 
-Object::Object(const std::string& textureName, Shader& shader, const float* vertices, unsigned const int* indices, unsigned int verticesCount) :
+Object::Object(const std::string& textureName, const Shader& shader, const float* vertices, unsigned const int* indices, unsigned int verticesCount) :
     m_textureName{textureName},
-    m_shader{shader},
-    m_verticesCount{verticesCount}
+    m_verticesCount{verticesCount},
+    m_shader{shader}
 {
     static const auto componentsCount = 8;
     
@@ -32,6 +32,40 @@ Object::Object(const std::string& textureName, Shader& shader, const float* vert
 	glBindVertexArray(0);
 
 	m_shader.Use();
+    RecalculateModelMatrix();
+}
+
+void Object::SetPosition(const glm::vec3& position)
+{
+    m_position = position;
+    RecalculateModelMatrix();
+}
+
+const glm::vec3& Object::GetPosition() const noexcept
+{
+    return m_position;
+}
+
+void Object::SetRotation(const glm::vec3& rotationInDegrees)
+{
+    m_rotationInDegrees = rotationInDegrees;
+    RecalculateModelMatrix();
+}
+
+const glm::vec3& Object::GetRotation() const noexcept
+{
+    return m_rotationInDegrees;
+}
+
+void Object::SetScale(const glm::vec3& scale)
+{
+    m_scale = scale;
+    RecalculateModelMatrix();
+}
+
+const glm::vec3& Object::GetScale() const noexcept
+{
+    return m_scale;
 }
 
 void Object::Destroy()
@@ -46,6 +80,7 @@ void Object::Draw()
     m_shader.Use();
     m_shader.Update();
     m_shader.SetColor(glm::vec3{1.0f, 1.0f, 1.0f});
+    m_shader.SetModelMatrix(m_modelMatrix);
     
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, ResourcesManager::GetInstance().EnsureTextureId(m_textureName));
@@ -53,3 +88,15 @@ void Object::Draw()
 	glBindVertexArray(VAO);
 	glDrawElements(GL_TRIANGLES, m_verticesCount, GL_UNSIGNED_INT, 0);
 }
+
+void Object::RecalculateModelMatrix()
+{
+    m_modelMatrix = glm::mat4{1.0f};
+    
+    m_modelMatrix = glm::translate(m_modelMatrix, m_position);
+    m_modelMatrix = glm::rotate(m_modelMatrix, glm::radians(m_rotationInDegrees.x), glm::vec3{1.0f, 0.0f, 0.0f});
+    m_modelMatrix = glm::rotate(m_modelMatrix, glm::radians(m_rotationInDegrees.y), glm::vec3{0.0f, 1.0f, 0.0f});
+    m_modelMatrix = glm::rotate(m_modelMatrix, glm::radians(m_rotationInDegrees.z), glm::vec3{0.0f, 0.0f, 1.0f});
+    m_modelMatrix = glm::scale(m_modelMatrix, m_scale);
+}
+
--- Source/Maze/Object.h
+++ Source/Maze/Object.h
@@ -17,17 +17,34 @@
 class Object
 {
 public:
-	Object(const std::string& textureName, Shader& shader, const float* vertices, const unsigned int* indices , unsigned int verticesCount);
+	Object(const std::string& textureName, const Shader& shader, const float* vertices, const unsigned int* indices , unsigned int verticesCount);
+    
+    void SetPosition(const glm::vec3& position);
+    const glm::vec3& GetPosition() const noexcept;
+    
+    void SetRotation(const glm::vec3& rotationInDegrees);
+    const glm::vec3& GetRotation() const noexcept;
+    
+    void SetScale(const glm::vec3& scale);
+    const glm::vec3& GetScale() const noexcept;
     
 	void Destroy();
 	void Draw();
     
 private:
+    void RecalculateModelMatrix();
+    
+private:
     const std::string m_textureName;
-    
-    Shader& m_shader;
-    unsigned int VBO, VAO, EBO;
-    
     const unsigned int m_verticesCount;
+    const Shader& m_shader;
+    
+    glm::vec3 m_position = glm::vec3{0.0f, 0.0f, 0.0f};
+    glm::vec3 m_rotationInDegrees = glm::vec3{0.0f, 0.0f, 0.0f};
+    glm::vec3 m_scale = glm::vec3{1.0f, 1.0f, 1.0f};
+    
+    glm::mat4 m_modelMatrix;
+    
+    unsigned int VBO, VAO, EBO;
 };
 
